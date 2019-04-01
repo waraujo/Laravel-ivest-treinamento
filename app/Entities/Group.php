3fe34252e5d538ce6ebffdbde8d62ede3c19fbf4
alexandro tiago de oliveira <alexandro.t.ol@gmail.com>
2019-03-31 21:58:05 -0300

    31-03-2019

---
+++ app/Entities/Group.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+/**
+ * Class Group.
+ *
+ * @package namespace App\Entities;
+ */
+class Group extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ 'nome','user_id','instituition_id'];
+/** funcção para criar relação com outras tabelas */
+    public function owner()
+    {
+    	return $this->belongTo(User::class);
+    }
+    public function instituition()
+    {
+    	return $this->belongTo(instituition::class);	
+    }
+}
